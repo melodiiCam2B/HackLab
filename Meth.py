6ba11a15e60ca3450f69da38c6af55c3418129e3
ChisanaHebi <115182751+ChisanaHebi@users.noreply.github.com>
2024-09-18 11:31:16 +0200

    norm + loop

---
+++ Meth.py
@@ -0,0 +1,14 @@
+
+i = input("Tafel van?")
+i2 = int(i)
+print(i2 * 1)
+print(i2 * 2)
+print(i2 * 3)
+print(i2 * 4)
+print(i2 * 5)
+print(i2 * 6)
+print(i2 * 7)
+print(i2 * 8)
+print(i2 * 9)
+print(i2 * 10)
+
---
+++ MethLoop.py
@@ -0,0 +1,5 @@
+i = input("Tafel van?")
+i2 = int(i)
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in num:
+    print(i2 * i)
