4b4d287682b0430719b23f205515769112a48554
ChisanaHebi <115182751+ChisanaHebi@users.noreply.github.com>
2024-09-18 10:26:28 +0200

    lol

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,16 @@
-print("print")
+text = "text!\ntext? \""
+strInt = 2
+intStr = "3"
+print("print: " + text)
+
+print(12 + 2)
+
+print(str(strInt) + " wow!")
+
+print("fuck " * 2)
+
+print(int(intStr) + 2)
+
+print(int(intStr) ** 2)
+
+print("world" >= 'world')
