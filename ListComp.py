c2410d1f9cf09625a785049f08591c5ff26bcc0e
melodiiCam2B <115182751+melodiiCam2B@users.noreply.github.com>
2024-10-09 10:28:17 +0200

    repush

---
+++ ListComp.py
@@ -0,0 +1,7 @@
+# list = "AbbyMelonStringEpicInfo"
+# relist = {"Abby":"A ", "Melon":"B ", "Epic":"C", "String":"D ", "Info":"E "}
+# listPrint = ""
+# for l in list:
+#     listPrint += relist[l]
+    
+# print(listPrint)
--- Replacement.py
+++ Replacement.py
@@ -1,9 +1,10 @@
-
 seq = "ATCATGCTGATAAACCTATA" 
 dict_seqflipper = {"A":"T", "T":"A", "C":"G", "G":"C"} 
 seq_flipped = "" 
 for l in seq: 
-  seq_flipped += dict_seqflipper[l] 
+  seq_flipped += dict_seqflipper[l]
   
+print(seq)
+print('|'*20)
 print(seq_flipped)
 
