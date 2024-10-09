531f1d1cb88a1632ed576bd13c7f2c9471785efe
melodiiCam2B <115182751+melodiiCam2B@users.noreply.github.com>
2024-10-09 09:43:53 +0200

    DNA

--- Replacement.py
+++ Replacement.py
@@ -1,9 +1,9 @@
-# Cytosine aan Guanine, en Adenine aan Thymine
-var = 'C G A T T G C A A'
 
-#new_var = var.replace("C", "G").replace("G", "C").replace("A", "T").replace("T", "A")
-
-new_var = var.split(' ')
-    
-print(new_var )
+seq = "ATCATGCTGATAAACCTATA" 
+dict_seqflipper = {"A":"T", "T":"A", "C":"G", "G":"C"} 
+seq_flipped = "" 
+for l in seq: 
+  seq_flipped += dict_seqflipper[l] 
+  
+print(seq_flipped)
 
