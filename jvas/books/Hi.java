1418e25d936529e891a64356ee00b5d3da56fac6
melodiiCam2B <115182751+melodiiCam2B@users.noreply.github.com>
2024-12-10 10:49:59 +0100

    re-push

---
+++ jvas/books/Hi.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Hi{
+    public static void main(String[] args) {
+        ArrayList<String>books = new ArrayList<String>();
+        books.add("Volvo");
+        books.add("BMW");
+        books.add("Ford");
+
+        System.out.println(books);
+    }
+}
