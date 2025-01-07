8bbf951cfd7959d68d566dd5be1fced25b0abcc5
melodiiCam2B <115182751+melodiiCam2B@users.noreply.github.com>
2025-01-07 10:17:59 +0100

    saa

--- jvas/books/Hi.java
+++ jvas/books/Hi.java
@@ -1,20 +1,5 @@
 import java.util.ArrayList;
 
 public class Hi{
-    public static void main(String[] args) {
-        ArrayList<String>books = new ArrayList<String>();
-        books.add("Volvo");
-        books.add("BMW");
-        books.add("Ford");
 
-        System.out.println(books);
-    }
-
-
-    public void showMainMenu(){}
-    public void handleAddBook(){}
-    public void showBookForm(){}
-    public void showAuthorMenu(){}
-    public void addBook(){}
-    public void showAllBooks(){}
 }
